971922c6fe6686d80a7d3392b3a5f4c3a2635933
Uche Egbon <ucheegbon904@gmail.com>
2022-08-22 22:31:31 +0100

    task 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import lasagne from './imgs/lasagne.jpg';
+import background from './imgs/background.jpeg';
+
+
+function Card(props){
+        return(
+          <div classname="card">
+            <div classname="cardBody">
+              <h2 classname="cardTitle"> {props.title} </h2>
+              <img src={props.img} alt=""></img>
+              <p>{props.description}</p>
+              <button classname="cardButton">Recipie!</button>
+            </div>
+          </div>
+        )
+}
+
+function Cards(){
+  const Lasagne = {title: "Lasagne", img: {lasagne}, description: " A rich and creamy whole-wheat pasta dish filled layer by layer with refreshingly fresh onions and garlic, lathered in a succulent sauce and topped with imported, premium quality mozzarella."}
+  return(
+    <>
+    <Card title = {Lasagne} img = {Lasagne} description = {Lasagne}></Card>
+    </>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="AppHeader">
+        <img src={background} className="background-img" alt="" />
+        <h1 className="heading">Uche's Recipie Book!</h1>
       </header>
+      <div className='cards'>
+        <Cards/>
+      </div>
     </div>
   );
 }
